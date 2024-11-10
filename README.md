4fb37c6e046ad65728fb11ec44cf8e017772abb0
mathiasbk <mathiasklow@gmail.com>
2024-11-10 20:02:51 +0100

    Added loading animation

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ But it is still work in progress.
 ## Roadmap
 - ~~Prefetching. Fetch the content when the user hovers the mouse over the links~~
 - ~~Update url when a tab is clicked ~~
+- ~~Maybe add a loading animation~~
 - Load correct tab on pageload
-- Maybe add a loading animation
 - Increase the speed more. Maybe use serviceworker to cache content of the tabs.
 
 
@@ -32,3 +32,4 @@ But it is still work in progress.
 | containerid   |              | DOM ID of the container that will show the tabcontent |
 | activetab     |              | Not yet implemented.                                  |
 | prefetching   | True / false | If prefetching should be enabled. Default is true.    |
+| loadinganimation   | True / false | Show loading animation. Default is true.    |
--- src/tabs.js
+++ src/tabs.js
@@ -14,12 +14,13 @@ class Tabs  {
         activetab: 0,
         tablinksclass: "tablink",
         containerid: "tab-container",
-        prefetching: true
+        prefetching: true,
+        loadinganimation: true
       };
 
       this.settings = { ...defaultOptions, ...options };
       this.container = document.getElementById(this.settings.containerid);
-      this.links = document.querySelectorAll(this.settings.tablinksilass);
+      this.links = document.querySelectorAll(this.settings.tablinksclass);
 
       if (!this.container) {
         console.error("Tab container not found");
@@ -30,14 +31,33 @@ class Tabs  {
         console.error("Tab links not found");
         return;
       }
+      
+      if(this.settings.loadinganimation)
+      {
+          // Add css animation for the loading animation
+        const styleSheet = document.createElement("style");
+        styleSheet.type = "text/css";
+        styleSheet.innerText = `
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `;
+        document.head.appendChild(styleSheet);
+      } 
 
       console.log(this.links);
 
       // Add event listeners for click
       this.links.forEach((link, index) => {
+        console.log("clicked", link);
         link.addEventListener("click", (event) => {
+          
 
           event.preventDefault();
+
+          //Show loadinganimation
+          this.LoadingAnimation(true);
 
           this.UpdateURL(link.textContent);
           if(!this.PrefetchedQued)
@@ -95,7 +115,11 @@ class Tabs  {
     {
       return fetch(pagelink)
         .then(response => response.text())
-        .then(data => { return data })
+        .then(data => 
+          { 
+            return data;
+          }
+        )
         .catch(error => console.error('Error:', error)
       );
     }
@@ -121,6 +145,8 @@ class Tabs  {
         this.UpdateContent(this.links[this.PrefetchedID], this.PrefetchedID);
       }
 
+      //Hide loadingamination
+
       return data;
     }
 
@@ -133,5 +159,30 @@ class Tabs  {
     Cleanurl(string)
     {
       return string.replace('[^.]*$', '');
+    }
+
+    LoadingAnimation(status)
+    {
+
+      var loadingdiv = document.createElement("div");
+      loadingdiv.classList.add("loadinganimation");
+      loadingdiv.style = `
+        display: block; 
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        animation: spin 1s linear infinite;
+        border: 16px solid #f3f3f3; /* Light grey */
+        border-top: 16px solid #3498db; /* Blue */
+        border-radius: 50%;
+        width: 80px;
+        height: 80px;
+        animation: spin 2s linear infinite;
+      `;
+
+      
+
+      this.container.appendChild(loadingdiv);
+
     }
 }
